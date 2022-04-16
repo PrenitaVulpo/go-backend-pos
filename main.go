dc7704d5b9bcdb81683a163575bbbd4dc41f1349
PrenitaVulpo <guilhermepdl97@protonmail.com>
2022-04-16 13:44:17 -0300

    add event struct

--- main.go
+++ main.go
@@ -1,4 +1,12 @@
+package main
+
 import "fmt"
+
+type Event struct {
+	Id          int    `json:"Id"`
+	Title       string `json:"title"`
+	Descriptiom string `json:"description"`
+}
 
 func main() {
 	fmt.Println("Starting API")
